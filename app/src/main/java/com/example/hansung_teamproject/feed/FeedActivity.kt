4ece190c6878a0136788a6cce06e11e92c80db75
sysliar <sysliar@gmail.com>
2022-11-16 17:41:07 +0900

    feat: activity 전환

--- app/src/main/java/com/example/hansung_teamproject/feed/FeedActivity.kt
+++ app/src/main/java/com/example/hansung_teamproject/feed/FeedActivity.kt
@@ -1,17 +1,15 @@
 package com.example.hansung_teamproject.feed
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.hansung_teamproject.FriendListActivity
+import com.example.hansung_teamproject.NewPostActivity
 import com.example.hansung_teamproject.R
 import com.example.hansung_teamproject.databinding.FeedBinding
-import com.example.hansung_teamproject.databinding.LoginBinding
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
+
 
 class FeedActivity : AppCompatActivity() {
     lateinit var binding: FeedBinding
@@ -26,5 +24,18 @@ class FeedActivity : AppCompatActivity() {
         binding.recyclerView.layoutManager = LinearLayoutManager(this@FeedActivity)
         binding.recyclerView.setHasFixedSize(true)
 
+        binding.topAppBar[1].setOnClickListener {
+            startActivity(Intent(this, NewPostActivity::class.java))
+        }
+
+        binding.topAppBar.setOnMenuItemClickListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.favorite -> {
+                    startActivity(Intent(this, FriendListActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
